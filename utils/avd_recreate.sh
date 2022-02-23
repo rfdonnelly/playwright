ca6981e4852204dc6f47e98df697a5fbc760655d
Max Schmitt <max@schmitt.mx>
2022-02-23 22:45:15 +0100

    back to roots

--- utils/avd_recreate.sh
+++ utils/avd_recreate.sh
@@ -10,5 +10,5 @@ fi
 
 ${ANDROID_HOME}/tools/bin/avdmanager delete avd --name android32 || true
 echo "y" | ${ANDROID_HOME}/tools/bin/sdkmanager --install "system-images;android-32;google_apis;x86_64"
-echo "no" | ${ANDROID_HOME}/tools/bin/avdmanager create avd --force --name android32 --device "pixel_xl" --package "system-images;android-32;google_apis;x86_64"
+echo "no" | ${ANDROID_HOME}/tools/bin/avdmanager create avd --force --name android32 --device "Nexus 5X" --package "system-images;android-32;google_apis;x86_64"
 ${ANDROID_HOME}/emulator/emulator -list-avds
